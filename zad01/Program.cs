102fd4b52b9f6a1ab00d4b63fc059c039ea7d8cb
max <mail.com>
2022-09-01 22:40:13 +0300

    Массив

---
+++ zad01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void ranmass()
+{
+    Console.WriteLine("Введите число элементов массива");
+        int N=Convert.ToInt32(Console.ReadLine());
+        int[] array = new int [N];
+        for (int i=0; i<N; i++)
+            {
+                Console.Write($"{array [i] = new Random().Next(0,100)}, ");  
+            }
+}
+ try 
+    {
+        ranmass();
+    }
+    catch
+    {
+        Console.WriteLine("Пожалуйта цифрами");
+    }
