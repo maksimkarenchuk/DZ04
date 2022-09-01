d988e0c81eac5a971df6b1f9c1eb7eeada09f447
max <mail.com>
2022-09-01 22:41:30 +0300

    Сумма чисел

---
+++ zad03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+string summa()
+{
+Console.WriteLine("Введите любое положительное число ");
+int a = Convert.ToInt32(Console.ReadLine());
+string str = a.ToString();
+int[] b = new int[str.Length];
+        for (int i=0; i< str.Length; i++)
+            {
+            b[i] = int.Parse(str[i].ToString());
+            // Console.Write($"{b[i]},  ");                //выводит массив
+            }
+           int sum = b.Sum();
+           return ($"Сумма чисел в числе {a} = {sum}");
+}
+    try 
+    {
+        Console.WriteLine(summa());
+    }
+    catch
+    {
+        Console.WriteLine("Пожалуйта цифрами");
+    }
